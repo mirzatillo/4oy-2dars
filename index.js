2508f833bafcb08655da3d267cf248d53fd5d21b
mirzatillo <ibragimovmirzatillo27@gamil.com>
2024-07-17 19:58:45 +0500

    vazifa

---
+++ index.js
@@ -0,0 +1,164 @@
+// let a = +prompt('sonni kiriting')
+
+// if (a>0) {
+//     a++
+// }
+// console.log(a);
+
+// 2masala
+
+// let a = +prompt('sonni kiriting')
+
+// if (a>0) {
+//     a++
+// }else{
+//     a-=2
+// }
+// console.log(a);
+
+// 3masala
+
+// let a = +prompt('sonni kiriting')
+
+// if (a>0) {
+//     a++
+// }else if (a==0) {
+//     a=10
+// }else{
+//     a-=2
+// }
+// console.log(a);
+
+// 4masala
+
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+// let c = +prompt('sonni kriting') 
+
+// let sum = 0
+
+// if (a>0) {
+//     sum++
+// }
+// if (b>0) {
+//     sum++
+// }
+// if (c>0) {
+//     sum++
+// }
+// console.log(sum);
+
+// 5masala
+
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+// let c = +prompt('sonni kriting')
+
+// let musbat =0
+// let manfiy =0
+
+// if (a>0) {
+//     musbat++
+// }else if (a<0) {
+//     manfiy++
+// }
+
+// if (b>0) {
+//     musbat++
+// }else if (b<0) {
+//     manfiy++
+// }
+
+// if (c>0) {
+//     musbat++
+// }else if (c<0) {
+//     manfiy++
+// }
+
+// console.log(musbat,'musbat');
+// console.log(manfiy,'manfiy');
+
+// 6masala
+
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+
+// let res=0;
+
+// if (a>b) {
+//     res=a
+// }else{
+//     res=b
+// }
+
+// console.log(res);
+
+// 7masala
+
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+
+// let res=0;
+
+// if (a<b) {
+//     res=a
+// }else{
+//     res=b
+// }
+
+// console.log(res);
+
+// 8masala
+
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+
+// let kata =0
+// let kichig =0
+
+// if (a>b) {
+//     kata=a
+// }else{
+//     kichig=a
+// }
+
+// if (a<b) {
+//     kata=b
+// }else{
+//     kichig=b
+// }
+
+// console.log(kata,'kata');
+// console.log(kichig,'kichig');
+
+// 9masala
+
+
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+
+// if (a>b) {
+//    b+=a
+// }
+
+// console.log(a,'a');
+// console.log(b,'b');
+
+// 10masal
+
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+
+// let res =0
+
+// if (a==b) {
+//     res+0
+// }
+
+// if (a!=b) {
+//     res=a+b
+// }
+
+// console.log(res);
+// console.log(a,'a');
+// console.log(b,'b');
